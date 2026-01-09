1e5e0ec134f88682eed903e20a239876c78e4963
ExoCode33 <emmanuel@supracode.ca>
2026-01-09 11:47:26 -0500

    Update profile.js

--- src/commands/user/profile.js
+++ src/commands/user/profile.js
@@ -1,5 +1,5 @@
 import database from '../../database/database.js';
-import storage from '../../services/storage.js';
+import storage from '../../services/image/storage.js';
 import embedUtils from '../../utils/embeds.js';
 
 export default {
@@ -9,34 +9,6 @@ export default {
     try {
       const userId = interaction.user.id;
       const guildId = interaction.guild.id;
-
-      // Get or create user record
-      let userResult = await database.query(
-        'SELECT * FROM users WHERE guild_id = $1 AND user_id = $2',
-        [guildId, userId]
-      );
-
-      if (userResult.rows.length === 0) {
-        // Create new user record
-        const configResult = await database.query(
-          'SELECT starting_elo FROM guild_config WHERE guild_id = $1',
-          [guildId]
-        );
-        
-        const startingElo = configResult.rows[0]?.starting_elo || 1000;
-
-        await database.query(
-          `INSERT INTO users (guild_id, user_id, elo) VALUES ($1, $2, $3)`,
-          [guildId, userId, startingElo]
-        );
-
-        userResult = await database.query(
-          'SELECT * FROM users WHERE guild_id = $1 AND user_id = $2',
-          [guildId, userId]
-        );
-      }
-
-      const user = userResult.rows[0];
 
       // Get user's top image
       const topImageResult = await database.query(
@@ -52,10 +24,10 @@ export default {
 
       if (topImageResult.rows.length > 0) {
         topImage = topImageResult.rows[0];
-        topImageUrl = await storage.getImageUrl(topImage.s3_key); // AWAIT added for signed URLs
+        topImageUrl = await storage.getImageUrl(topImage.s3_key);
       }
 
-      const embed = embedUtils.createProfileEmbed(user, topImage, topImageUrl);
+      const embed = embedUtils.createProfileEmbed(null, topImage, topImageUrl);
 
       await interaction.editReply({ embeds: [embed] });
     } catch (error) {
