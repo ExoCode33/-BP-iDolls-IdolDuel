6b28eea1085769eba8bd00daa1a02cd8584bacd8
ExoCode33 <emmanuel@supracode.ca>
2026-01-09 10:33:40 -0500

    Update selectHandler.js

--- src/handlers/selectHandler.js
+++ src/handlers/selectHandler.js
@@ -1,8 +1,85 @@
 import imageManagement from '../commands/admin/imageManagement.js';
+import adminConfig from '../commands/admin/config.js';
+import adminLogs from '../commands/admin/adminLogs.js';
 import database from '../database/database.js';
+import embedUtils from '../utils/embeds.js';
+import { MessageFlags } from 'discord.js';
 
 export async function handleSelectMenu(interaction) {
   const customId = interaction.customId;
+
+  // Admin menu navigation
+  if (customId === 'admin_menu_select') {
+    const value = interaction.values[0];
+    const config = await database.query(
+      'SELECT * FROM guild_config WHERE guild_id = $1',
+      [interaction.guild.id]
+    );
+    
+    const guildConfig = config.rows[0];
+    
+    await interaction.deferUpdate();
+    
+    if (value === 'quick_setup') {
+      await adminConfig.showQuickSetup(interaction, guildConfig);
+    } else if (value === 'approve_images') {
+      // TODO: Show approval queue
+      await interaction.followUp({
+        content: 'Approval queue coming soon!',
+        flags: MessageFlags.Ephemeral
+      });
+    } else if (value === 'schedule_settings') {
+      await adminConfig.showSchedulePresets(interaction, guildConfig);
+    } else if (value === 'import_settings') {
+      await adminConfig.showImportSettings(interaction, guildConfig);
+    } else if (value === 'retirement_settings') {
+      await adminConfig.showRetirementSettings(interaction, guildConfig);
+    } else if (value === 'advanced_settings') {
+      await adminConfig.showAdvancedSettings(interaction, guildConfig);
+    } else if (value === 'view_logs') {
+      await adminLogs.showLogs(interaction);
+    }
+    
+    return;
+  }
+
+  // Schedule presets
+  if (customId === 'schedule_preset_select') {
+    const value = interaction.values[0];
+    await interaction.deferUpdate();
+    
+    let duration, interval;
+    
+    if (value === 'preset_6h') {
+      duration = interval = 21600; // 6 hours
+    } else if (value === 'preset_12h') {
+      duration = interval = 43200; // 12 hours
+    } else if (value === 'preset_24h') {
+      duration = interval = 86400; // 24 hours
+    } else if (value === 'preset_custom') {
+      await interaction.showModal(adminConfig.createDuelDurationModal());
+      return;
+    }
+    
+    await database.query(
+      'UPDATE guild_config SET duel_duration = $1, duel_interval = $2 WHERE guild_id = $3',
+      [duration, interval, interaction.guild.id]
+    );
+    
+    const successEmbed = embedUtils.createSuccessEmbed(
+      `Schedule updated!\n\n` +
+      `Duels will run every ${duration / 3600} hours for ${duration / 3600} hours.`
+    );
+    
+    await interaction.followUp({ embeds: [successEmbed], flags: MessageFlags.Ephemeral });
+    
+    const config = await database.query(
+      'SELECT * FROM guild_config WHERE guild_id = $1',
+      [interaction.guild.id]
+    );
+    await adminConfig.showMainMenu(interaction, config.rows[0]);
+    return;
+  }
 
   // Browse images - sort menu
   if (customId === 'browse_images_sort') {
